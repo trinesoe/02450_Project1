bd1cd2c9b79d1e462a6a922260b1604cc5a74dea
CLNielsen9 <caroline@lenarth.dk>
2025-04-08 18:14:44 +0200

    Cleaning out notes

--- Regression_part_A.py
+++ Regression_part_A.py
@@ -36,16 +36,14 @@ attribute_names = [u'Offset']+attribute_names
 M = M+1
 
 ## Crossvalidation
-# Create crossvalidation partition for evaluation
 K = 10
 CV = model_selection.KFold(K, shuffle=True)
-#CV = model_selection.KFold(K, shuffle=False)
+
 
 # Values of lambda
 lambdas = np.power(10.,range(-5,9))
 
 # Initialize variables
-#T = len(lambdas)
 Error_train = np.empty((K,1))
 Error_test = np.empty((K,1))
 Error_train_rlr = np.empty((K,1))
@@ -74,16 +72,7 @@ for train_index, test_index in CV.split(X,y):
     X_train[:, 1:] = (X_train[:, 1:] - mu[k, :]) / sigma[k, :]
     X_test[:, 1:] = (X_test[:, 1:] - mu[k, :]) / sigma[k, :]
       
-    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation)
-
-    # Standardize outer fold based on training set, and save the mean and standard
-    # deviations since they're part of the model (they would be needed for
-    # making new predictions) - for brevity we won't always store these in the scripts
-    # mu[k, :] = np.mean(X_train[:, 1:], 0)
-    # sigma[k, :] = np.std(X_train[:, 1:], 0)
-    
-    # X_train[:, 1:] = (X_train[:, 1:] - mu[k, :] ) / sigma[k, :] 
-    # X_test[:, 1:] = (X_test[:, 1:] - mu[k, :] ) / sigma[k, :] 
+    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, internal_cross_validation) 
     
     Xty = X_train.T @ y_train
     XtX = X_train.T @ X_train
@@ -97,17 +86,11 @@ for train_index, test_index in CV.split(X,y):
     lambdaI = opt_lambda * np.eye(M)
     lambdaI[0,0] = 0 # Do no regularize the bias term
     w_rlr[:,k] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
-    #  Compute mean squared error with regularization with optimal lambda
-    #  Error_train_rlr[k] = np.square(y_train-X_train @ w_rlr[:,k]).sum(axis=0)/y_train.shape[0]
-    #  Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
 
     # Unregularized linear regression
     # Estimate weights for unregularized linear regression, on entire training set
     w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
-    # Compute mean squared error without regularization
-    #  Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
-    #  Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
-    # OR ALTERNATIVELY: you can use sklearn.linear_model module for linear regression:
+
     m = lm.LinearRegression().fit(X_train, y_train)
     Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
     Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
@@ -122,8 +105,6 @@ for train_index, test_index in CV.split(X,y):
         plt.xlabel("Regularization factor")
         plt.ylabel("Mean Coefficient Values")
         plt.grid()
-        # You can choose to display the legend, but it's omitted for a cleaner
-        # plot, since there are many attributes
         plt.legend(attributeNames[1:], loc='best')
 
         plt.subplot(1, 2, 2)
@@ -135,14 +116,11 @@ for train_index, test_index in CV.split(X,y):
         plt.ylabel("Squared error (crossvalidation)")
         plt.legend(["Train error", "Validation error"])
         plt.grid()
-    # To inspect the used indices, use these print statements
-    #print('Cross validation fold {0}/{1}:'.format(k+1,K))
-    #print('Train indices: {0}'.format(train_index))
-    #print('Test indices: {0}\n'.format(test_index))
+    
 
 
 plt.show()
-# Display results
+
 print('Linear regression without feature selection:')
 print('- Training error: {0}'.format(Error_train.mean()))
 print('- Test error:     {0}'.format(Error_test.mean()))
